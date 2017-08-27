623e101d4fa23027abf1f367905dcaf8a71dc02b
grechny <grechny@vmsis.by>
2017-08-27 12:19:34 +0300

    Changed version

--- src/main/java/by/sysadmins/callscript/CallScriptApplication.java
+++ src/main/java/by/sysadmins/callscript/CallScriptApplication.java
@@ -2,13 +2,9 @@ package by.sysadmins.callscript;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-//for jsr310 java 8 java.time.*
-@EntityScan(basePackageClasses = {CallScriptApplication.class, Jsr310JpaConverters.class})
 @SpringBootApplication
 public class CallScriptApplication extends SpringBootServletInitializer {
 
@@ -21,4 +17,3 @@ public class CallScriptApplication extends SpringBootServletInitializer {
     return application.sources(CallScriptApplication.class);
   }
 }
-
